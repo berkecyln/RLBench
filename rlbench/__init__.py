eb59115a30138a40b3cf2cb1f00c08aeaaba346d
berkecyln <berkeceylan20@hotmail.com>
2026-05-02 18:18:42 +0200

    make pyrep import conditional

--- rlbench/__init__.py
+++ rlbench/__init__.py
@@ -1,17 +1,21 @@
 __version__ = '1.2.0'
 
-import numpy as np
-import pyrep
+# pyrep/CoppeliaSim not required for data conversion (prepare_data_for_peract.py).
+# Simulation-dependent imports are guarded so the package can be used headlessly.
+try:
+    import numpy as np
+    import pyrep
 
-pr_v = np.array(pyrep.__version__.split('.'), dtype=int)
-if pr_v.size < 4 or np.any(pr_v < np.array([4, 1, 0, 2])):
-    raise ImportError(
-        'PyRep version must be greater than 4.1.0.2. Please update PyRep.')
+    pr_v = np.array(pyrep.__version__.split('.'), dtype=int)
+    if pr_v.size < 4 or np.any(pr_v < np.array([4, 1, 0, 2])):
+        raise ImportError(
+            'PyRep version must be greater than 4.1.0.2. Please update PyRep.')
 
-
-from rlbench.environment import Environment
-from rlbench.action_modes.action_mode import ActionMode, ArmActionMode, GripperActionMode
-from rlbench.observation_config import ObservationConfig
-from rlbench.observation_config import CameraConfig
-from rlbench.sim2real.domain_randomization import RandomizeEvery
-from rlbench.sim2real.domain_randomization import VisualRandomizationConfig
+    from rlbench.environment import Environment
+    from rlbench.action_modes.action_mode import ActionMode, ArmActionMode, GripperActionMode
+    from rlbench.observation_config import ObservationConfig
+    from rlbench.observation_config import CameraConfig
+    from rlbench.sim2real.domain_randomization import RandomizeEvery
+    from rlbench.sim2real.domain_randomization import VisualRandomizationConfig
+except ModuleNotFoundError:
+    pass
